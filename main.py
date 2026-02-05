b97c8edc38cffbc4832e38f0fec4579c43b3edf4
EverQuintEzhil <ezhilraj.selvaraj@everquint.com>
2026-02-05 17:16:44 +0530

    Revert "added marketing toolkit"

--- main.py
+++
@@ -1,6 +0,0 @@
-from src.pipeline import Pipeline
-import asyncio
-
-obj = Pipeline()
-
-asyncio.run(obj.run("63241638417ca879fd2f76d0"))
--- src/text_processing/get_token_length.py
+++
@@ -1,5 +0,0 @@
-import tiktoken
-
-def count_tokens(text: str, model: str = "text-embedding-3-large") -> int:
-    encoding = tiktoken.get_encoding("cl100k_base")
-    return len(encoding.encode(text))
